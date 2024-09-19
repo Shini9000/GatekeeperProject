bfbb95392e2674a0eb01f2e78a7c7c32ff390085
Aidan Christie <aidanchristie2232@gmail.com>
2024-09-19 03:19:27 +0100

    1.1
    Will ask for admin code - but wont progress yet. Will convert to file reading in 1.2

---
+++ README.md
@@ -0,0 +1,46 @@
+# Gatekeeper prototype v1.1
+
+## Choose an option:
+> #### 1. Generate Password
+> Allows you to create a password
+> 1. Uppercase # ABC
+> 2. Lowercase # abc
+> 3. Numbers # 123
+> 4. Mix # Ab3
+> 5. GenericPassword (Cap,Num and Sym # Ab3!)
+>
+> Additionally, you can choose to save password (Not implemented in this version)
+
+> #### 2.View Passwords
+> Not implemented yet, but will allow you to see
+> Shows you all passwords as a list in the txt document.
+
+> #### 3. View Accounts
+> Not implemented yet, but will allow you to see
+> Email, Name and Password (Keys (Not implemented) used for that account
+
+> #### 4. Add Account
+> Not yet implemented but will allow you to create an account using
+> Email, Name and Password (Key (Not implemented)
+
+> #### 5. Delete Account
+> Not implemented yet, but will allow you to delete an account...
+
+> #### 6. Help menu
+> Opens the help menu
+
+> #### 7. Options
+> Opens application options
+
+> #### 8. Exit
+> Exits the program
+
+-----
+## Imports
+As of version 1.1:
+- *string*
+- *random*
+- *time*
+-----
+GitHub repo: [Gatekeeper](https://github.com/Shini9000/PasswordManagerApplication)<br>
+Author: [Linktree](https://linktr.ee/shini9000) / [Linkedin](https://www.linkedin.com/in/aidanchristie98) /
--- main.py
+++ main.py
@@ -1,1 +1,47 @@
-print("Hello world!")
+# variables for settings mainly
+import time
+
+projectVersion = 1.0
+adminCode = ""
+adminLoggedin = False
+
+
+def adminLogin():
+    if adminCode == "":
+        print("First time setup!\n"
+              "You must enter a admin password or passcode.")
+        time.sleep(2)
+        adminCode == input("Enter new Admin code: ")
+        print(adminCode)
+    else:
+        if input("Enter Admin code: ") == adminCode:
+            print("Logging in")
+            adminLoggedin == True
+            main(adminLoggedin)
+        else:
+            print("Wrong passcode!")
+            adminLogin()
+
+
+def main(adminLoggedin):
+    if adminLoggedin == False:
+        print("Not logged in as admin")
+        time.sleep(1)
+        adminLogin()
+    else:
+        while True:
+            time.sleep(2.5)
+            #Main menu loop
+            mainMenu_Input = input(f"Choose and option: "
+                                   f"\n\t1. Generate a password"
+                                   f"\n\t2. View passwords"
+                                   f"\n\t3. View accounts"
+                                   f"\n\t4. Add account"
+                                   f"\n\t5. Delete account"
+                                   f"\n\t6. Show help "
+                                   f"\n\t7. Options"
+                                   f"\n\t8. Exit"
+                                   f"\n\nEnter input: ")
+
+
+main(adminLoggedin)
